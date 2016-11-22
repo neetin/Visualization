2e60af57b2cb7e636e295bc2b5a5e785e11a2ba9
vsujan <sujanvaidya@lftechnology.com>
2016-11-22 12:40:17 +0545

    Added fill color

--- GraphTest/DesatVC.swift
+++ GraphTest/DesatVC.swift
@@ -11,6 +11,11 @@ import Charts
 
 class DesatVC: UIViewController, ChartViewDelegate {
 
+    let dayColor = UIColor(colorLiteralRed: 203/255, green: 235/255, blue: 247/255, alpha: 0.5)//UIColor(hex: "cbebf7")
+    let nightColor = UIColor(colorLiteralRed: 220/255, green: 204/255, blue: 241/255, alpha: 0.5)//UIColor(hex: "dcccf1")
+    let lineColor = UIColor(colorLiteralRed: 76/255, green: 185/255, blue: 228/255, alpha: 1)//UIColor(hex: "28a9df")
+//    let borderColor = UIColor(colorLiteralRed: 174/255, green: 204/255, blue: 243/255, alpha: 1)
+    
     @IBOutlet weak var chartView: CombinedChartView!
     weak var valueFormatter: IAxisValueFormatter!
     
@@ -41,15 +46,21 @@ class DesatVC: UIViewController, ChartViewDelegate {
         }
         
         let candleChartDataSet = CandleChartDataSet(values: dataEntries1, label: "Company 1")
-        candleChartDataSet.colors = [UIColor.red, UIColor.green]
-        candleChartDataSet.shadowColor = UIColor.white
+        
+        candleChartDataSet.colors = [dayColor, nightColor]
+        candleChartDataSet.shadowColor = UIColor.clear
+//        candleChartDataSet.shadowColorSameAsCandle = true
         candleChartDataSet.drawValuesEnabled = false
         candleChartDataSet.highlightEnabled = false
+        candleChartDataSet.fillBackground = true
         
         let lineChartDataSet = LineChartDataSet(values: dataEntries2, label: "Company 2")
-        lineChartDataSet.colors = [UIColor.green]
+        lineChartDataSet.colors = [lineColor]
         lineChartDataSet.circleHoleRadius = 0
+        lineChartDataSet.circleColors = [lineColor]
         lineChartDataSet.highlightEnabled = false
+        lineChartDataSet.valueColors = [lineColor]
+        lineChartDataSet.valueFont = UIFont.systemFont(ofSize: 18)
         
         let rightYAxis = chartView.getAxis(.right)
         rightYAxis.axisMaximum = 25 // based on the desaturation values
@@ -62,19 +73,25 @@ class DesatVC: UIViewController, ChartViewDelegate {
         let xAxis = chartView.xAxis
         xAxis.valueFormatter = valueFormatter
         xAxis.labelPosition = .bottom
-        xAxis.axisMinimum = -0.5 // Fixes for candlestick
-        xAxis.axisMaximum = 13.5 // Fixes for candlestick
+        
+        xAxis.spaceMin = 0.5
+        xAxis.spaceMax = 0.5
+        
+//        xAxis.axisMinimum = -0.5 // Fixes for candlestick
+//        xAxis.axisMaximum = 13.5 // Fixes for candlestick
 //        xAxis.forceLabelsEnabled = true
 //        xAxis.centerAxisLabelsEnabled = true
-        xAxis.entries = [1.5, 3.5, 5.5, 7.5, 9.5, 11.5]
+//        xAxis.entries = [1.5, 3.5, 5.5, 7.5, 9.5, 11.5]
         //        xAxis.entries = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
         
         let chartData = CombinedChartData()
         chartData.candleData = CandleChartData(dataSet: candleChartDataSet)
         chartData.lineData = LineChartData(dataSet: lineChartDataSet)
         chartView.data = chartData
+//        chartView.drawBordersEnabled = true
+//        chartView.borderColor = borderColor
         
-        chartView.drawOrder = [DrawOrder.line.rawValue, DrawOrder.candle.rawValue]
+        chartView.drawOrder = [DrawOrder.candle.rawValue, DrawOrder.line.rawValue]
         chartView.chartDescription?.text = ""
         
         // unnecessary gesture
